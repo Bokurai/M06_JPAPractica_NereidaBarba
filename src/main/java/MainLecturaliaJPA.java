3b090ef6146d97f6eb79ca608b284e9920ec4b17
Bokurai <mrnere88@gmail.com>
2024-02-15 16:49:31 +0100

    especificando las tablas a través de las clases

--- src/main/java/MainLecturaliaJPA.java
+++ src/main/java/MainLecturaliaJPA.java
@@ -1,4 +1,39 @@
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class MainLecturaliaJPA {
+
+    static SessionFactory sessionFactoryObj;
+
+    private static SessionFactory buildSessionFactory() {
+        try {
+            StandardServiceRegistry standardRegistry = new StandardServiceRegistryBuilder()
+                    .configure("hibernate.cfg.xml").build();
+            Metadata metadata = new MetadataSources(standardRegistry).getMetadataBuilder().build();
+            return metadata.getSessionFactoryBuilder().build();
+        } catch (HibernateException he) {
+            System.out.println("Session Factory creation failure");
+            throw he;
+        }
+    }
+
+    public static EntityManagerFactory createEntityManagerFactory() {
+        EntityManagerFactory emf;
+        try {
+            emf = Persistence.createEntityManagerFactory("JPALecturalia");
+        } catch (Throwable ex) {
+            System.err.println("Failed to create EntityManagerFactory object." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+        return emf;
+    }
 
     public static void main(String[] args) {
 
